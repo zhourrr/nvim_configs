b2270af5b06caed94d099c61346825a221f1edbd
zhourrr <3107745933@qq.com>
2022-12-03 17:38:43 -0600

    upgraded! version 1.0

--- init.lua
+++ init.lua
@@ -45,18 +45,19 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-require("packer").startup(function(use)     -- install plugins
+require("packer").startup(function(use)             -- install plugins
     use "wbthomason/packer.nvim"
-    use "EdenEast/nightfox.nvim"            -- colorscheme
-    use "ggandor/leap.nvim"            	    -- easymotion
+    use "EdenEast/nightfox.nvim"                    -- colorscheme
+    use "ggandor/leap.nvim"            	            -- easymotion
     -- Language Server Protocol (LSP)
-    use "nvim-treesitter/nvim-treesitter"   
-    use "williamboman/mason.nvim"           -- LSP servers manager
-    use "williamboman/mason-lspconfig.nvim" -- helper for mason.nvim
-    use "neovim/nvim-lspconfig"             -- Nvim LSP client configs 
+    use "nvim-treesitter/nvim-treesitter"           -- highlight
+    use "nvim-treesitter/nvim-treesitter-context"   -- sticky scroll
+    use "williamboman/mason.nvim"                   -- LSP servers manager
+    use "williamboman/mason-lspconfig.nvim"         -- helper for mason.nvim
+    use "neovim/nvim-lspconfig"                     -- Nvim LSP client configs 
     use {
-        "hrsh7th/nvim-cmp",                 -- autocompletion engine
-        requires = {                        -- autocompletion sources
+        "hrsh7th/nvim-cmp",                         -- autocompletion engine
+        requires = {                                -- autocompletion sources
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-nvim-lsp-signature-help",
             "hrsh7th/cmp-buffer",
@@ -69,12 +70,13 @@ require("packer").startup(function(use)     -- install plugins
     -- file explorer
     use {
         "nvim-tree/nvim-tree.lua",
-        requires = { 'nvim-tree/nvim-web-devicons' }    -- optional, for file icons
+        requires = {'nvim-tree/nvim-web-devicons'}  -- optional, for file icons
     }
     use { 
         "nvim-telescope/telescope.nvim",
         requires = { "nvim-lua/plenary.nvim" }
     }
+    use "lewis6991/gitsigns.nvim"                   -- git integration
 
     -- put this at the end after all plugins
     if packer_bootstrap then
@@ -112,18 +114,18 @@ require("mason-lspconfig").setup {
         "clangd",
         --"cmake",
         "rust_analyzer",
-        --"pyright"
+        --"pylsp"
     }
 }
 
-require('telescope').setup {        -- telescope: previewer
+require('telescope').setup {        -- telescope: picker and previewer
    defaults = {
         layout_strategy = "horizontal",
         layout_config = {
             width = 0.9,            -- floating window takes up 90% of the screen
             preview_width = 0.5     -- preview window takes up 50% of the floating window 
         },
-        initial_mode = "normal",
+        initial_mode = "normal",    -- starts in normal mode, press i to enter insert mode
         mappings = {
             n = {
                 -- kill selected buffer in buffer picker
@@ -141,6 +143,7 @@ local on_attach = function(client, bufnr)   -- has effect only if the language s
 
     -- auto format on save
     cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+    -- lsp services
     nmap('gd', '<cmd>Telescope lsp_definitions<CR>')
     nmap('gr', '<cmd>Telescope lsp_references<CR>')
     nmap('gt', '<cmd>Telescope lsp_type_definitions<CR>')
@@ -156,9 +159,13 @@ require "lspconfig".clangd.setup{ on_attach = on_attach }
 require "lspconfig".pyright.setup{ on_attach = on_attach }
 
 -- set up autocompletion engine
-opt.completeopt = menu, menuone, noselect
+opt.completeopt = { "menu", "menuone", "noselect" }             -- autocompletion menu
 local cmp = require("cmp")
 local select_opts = { behavior = cmp.SelectBehavior.Select }
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup {
     mapping = {
@@ -166,22 +173,57 @@ cmp.setup {
         [ "<Up>" ] = cmp.mapping.select_prev_item(select_opts),
         [ "<Down>" ] = cmp.mapping.select_next_item(select_opts),
         [ "<Tab>" ] = cmp.mapping(function(fallback)
-            if cmp.visible() then
+            if cmp.visible() then       -- tab scrolls down
                 cmp.select_next_item(select_opts)
-            else
+            elseif has_words_before() then          
                 cmp.complete()
+            else
+                fallback()              -- don't complete for an empty word
             end
-        end)
+        end, { "i", "c" })              -- applied to insert and command mode
     },
-    sources = { -- the order affects the priority
-        { name = "nvim_lsp_signature_help", priority = 8 },
-        { name = "nvim-lsp", keyword_length = 2, priority = 8 },
-        { name = "path" , keyword_length = 2, priority = 1 },
-        { name = "buffer", keyword_length = 2, priority = 1 }
+    sources = { -- the order might affect the priority 
+                -- keyword length controls how many characters are necessary to begin querying the source
+        { name = "nvim_lsp_signature_help", keyword_length = 1, priority = 8 },
+        { name = "nvim_lsp", keyword_length = 3, priority = 7 },
+        { name = "path" , keyword_length = 4, priority = 1 },
+        { name = "buffer", keyword_length = 4, priority = 1 }
+    },
+    formatting = {  -- autocompletion menu format
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            vim_item.kind = string.format("%s", vim_item.kind)  -- what kind of object is it?
+            vim_item.menu = ({                                  -- source name
+                nvim_lsp_signature_help = "[LSP_sig]",
+                nvim_lsp = "[LSP]",
+                buffer = "[Buffer]",
+                path = "[Path]"
+	        })[entry.source.name]
+	    return vim_item
+	    end
+    },
+    window = {  -- I like borders
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
     }
 }
 
-require("nvim-tree").setup {    -- file explorer
+cmp.setup.cmdline({ "/", "?" }, {           -- in-file search should use source: buffer
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" }
+    }
+})
+cmp.setup.cmdline(":", {                    -- commandline autocompletion uses special sources
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" }
+    }, {
+        { name = "cmdline" }
+    })
+})
+
+require("nvim-tree").setup {    -- file explorer, see below for mappings
     sort_by = "case_sensitive",
     view = {
         adaptive_size = true    -- the side bar size changes as needed
@@ -194,9 +236,21 @@ require("nvim-tree").setup {    -- file explorer
     }
 }
 
+require("gitsigns").setup {
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        -- navigation
+        nmap("<Leader>n", gs.next_hunk)
+        nmap("<Leader>p", gs.prev_hunk)
+        -- preview 
+        nmap("<Leader>v", gs.preview_hunk)
+    end
+}
+
 
 --
--- configurations
+-- general configurations
 --
 opt.number = true
 opt.relativenumber = true
@@ -214,7 +268,7 @@ opt.smartcase = true        -- works as case-insensitive if you only use lowerca
 opt.cursorline = true       -- highlight the cursorline
 opt.termguicolors = true    -- true color support
 opt.wrap = true             -- wrap very long lines to make them look like multiple lines
-opt.textwidth = 120         -- character limits in one line
+opt.textwidth = 120         -- the upper limit of the number of characters in one line
 opt.autoindent = true
 opt.expandtab = true
 opt.tabstop = 4
@@ -227,7 +281,7 @@ opt.clipboard = "unnamedplus"
 -- display file name on the terminal title
 opt.title = true
 
--- turn backup off, since most stuff is in Git or else anyway...
+-- turn backup off, since most stuff is in Git or something else anyway...
 opt.backup = false
 opt.wb = false
 opt.swapfile = false
@@ -250,9 +304,9 @@ nmap("<Leader>tf", "<cmd>Telescope find_files<cr>")     -- search for files in t
 nmap("<Leader>tg", "<cmd>Telescope live_grep<cr>")      -- search for strings in the current working directory
 nmap("<Leader>tb", "<cmd>Telescope buffers<cr>")        -- list opened files (buffers)
 nmap("<Leader>to", "<cmd>Telescope oldfiles<cr>")       -- list recently opened files
--- nvim-tree
--- open a file: <C-v> vsplit, <C-x> split
--- r: rename; a: create; d: remove
+-- nvim-tree, you can actually use your mouse!
+-- <Enter> open a file; <C-v> vsplit; <C-x> split;
+-- r: rename; a: create; d: remove; f: create a live filter; F: clear the live filter; H: toggle dotfiles
 nmap("<C-b>", ":NvimTreeFindFileToggle<CR>")            -- toogle file tree
 nmap("<C-z>", ":NvimTreeCollapse<CR>")                  -- collapses the nvim-tree recursively
 
